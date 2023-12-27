1cef07e88263f07e7232f06a113e9641fbd7f83a
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2023-12-28 02:01:42 +0700

    add sealed class KontakUIState

--- app/src/main/java/com/example/restapi/ui/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/restapi/ui/home/viewmodel/HomeViewModel.kt
@@ -1,2 +1,10 @@
 package com.example.restapi.ui.home.viewmodel
 
+import com.example.restapi.model.Kontak
+
+sealed class KontakUIState {
+    data class Success(val kontak: List<Kontak>) : KontakUIState()
+    object Error : KontakUIState()
+    object Loading : KontakUIState()
+}
+
