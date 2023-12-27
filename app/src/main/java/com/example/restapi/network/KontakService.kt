ae7f25be6c743e9a8ffa66981c64b753515fd664
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2023-12-28 00:00:50 +0700

    add get,post, put, delete with KontakService

--- app/src/main/java/com/example/restapi/network/KontakService.kt
+++ app/src/main/java/com/example/restapi/network/KontakService.kt
@@ -1,8 +1,14 @@
 package com.example.restapi.network
 
 import com.example.restapi.model.Kontak
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
 
 interface KontakService {
     @Headers(
@@ -10,4 +16,15 @@ interface KontakService {
     )
     @GET("siswa.json")
     suspend fun getKontak(): List<Kontak>
+    @GET("/kontak/{id}")
+    suspend fun getKontakById(@Path("id") id: Int): Kontak
+
+    @POST("/kontak")
+    suspend fun insertKontak(@Body kontak: Kontak)
+
+    @PUT("/kontak/{id}")
+    suspend fun updateKontak(@Path("id") id: Int, @Body kontak: Kontak)
+
+    @DELETE("/kontak/{id}")
+    suspend fun deleteKontak(@Path("id") id: Int): Response<Void>
 }
