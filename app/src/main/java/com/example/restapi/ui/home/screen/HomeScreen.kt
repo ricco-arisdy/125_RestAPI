20255b41cc112f298801181aa6866b8beb82c0b2
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2023-12-28 02:23:02 +0700

    add fun KontakCard in HomeScreen

--- app/src/main/java/com/example/restapi/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/restapi/ui/home/screen/HomeScreen.kt
@@ -1,9 +1,27 @@
 package com.example.restapi.ui.home.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.restapi.model.Kontak
 import com.example.restapi.ui.PenyediaViewModel
 import com.example.restapi.ui.viewmodel.HomeViewModel
 
@@ -17,4 +35,19 @@ fun  HomeScreen(
     viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ){
 
+}
+
+@Composable
+fun KontakCard(
+    kontak: Kontak,
+    onDeleteClick: (Kontak) -> Unit = {},
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier,
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+
+    }
 }
