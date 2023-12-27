9837982a9926396d3ef4f338898a6090672e9b83
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2023-12-28 02:29:39 +0700

    add row and coulm in fun KontakCard

--- app/src/main/java/com/example/restapi/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/restapi/ui/home/screen/HomeScreen.kt
@@ -48,6 +48,56 @@ fun KontakCard(
         shape = MaterialTheme.shapes.medium,
         elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
     ) {
+        @Composable
+        fun KontakCard(
+            kontak: Kontak,
+            onDeleteClick: (Kontak) -> Unit = {},
+            modifier: Modifier = Modifier
+        ) {
+            Card(
+                modifier = modifier,
+                shape = MaterialTheme.shapes.medium,
+                elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+            ) {
+                Column(
+                    modifier = Modifier.padding(16.dp),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                    ) {
+                        Text(
+                            text = kontak.nama,
+                            style = MaterialTheme.typography.titleLarge,
+                        )
+                        Spacer(Modifier.weight(1f))
+                        Icon(
+                            imageVector = Icons.Default.Phone,
+                            contentDescription = null,
+                        )
+                        Text(
+                            text = kontak.nohp,
+                            style = MaterialTheme.typography.titleMedium
+                        )
+                    }
+                    Row(modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = kontak.email,
+                            style = MaterialTheme.typography.titleMedium
+                        )
+                        Spacer(Modifier.weight(1f))
+                        IconButton(onClick = {onDeleteClick(kontak) }) {
+                            Icon(
+                                imageVector = Icons.Default.Delete,
+                                contentDescription = null,
+                            )
+                        }
+                    }
+                }
 
+            }
+        }
     }
 }
