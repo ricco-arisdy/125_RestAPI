d79ebd379353c397e0572a27291be1ca007597e0
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2023-12-28 16:42:43 +0700

    add packges kontak, function EntryKontakScreen and insertviewmodel

---
+++ app/src/main/java/com/example/restapi/ui/kontak/screen/InsertScreen.kt
@@ -0,0 +1,19 @@
+package com.example.restapi.ui.kontak.screen
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import com.example.restapi.ui.TopAppBarKontak
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+
+fun EntryKontakScreen(
+
+){
+
+}
+
+
---
+++ app/src/main/java/com/example/restapi/ui/kontak/viewmodel/InsertViewModel.kt
@@ -0,0 +1,58 @@
+package com.example.restapi.ui.kontak.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.restapi.model.Kontak
+import com.example.restapi.repository.KontakRepository
+import kotlinx.coroutines.launch
+
+class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel() {
+    var insertKontakState by mutableStateOf(InsertUiState())
+        private set
+
+    fun updateInsertKontakState(insertUiEvent: InsertUiEvent) {
+        insertKontakState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertKontak() {
+        viewModelScope.launch {
+            try {
+                kontakRepository.insertKontak(insertKontakState.insertUiEvent.toKontak())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
+
+data class InsertUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent()
+)
+
+data class InsertUiEvent(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val nohp: String = ""
+)
+
+fun InsertUiEvent.toKontak(): Kontak = Kontak(
+    id =id,
+    nama = nama,
+    email = alamat,
+    nohp = nohp
+)
+
+fun Kontak.toUiStateKontak(): InsertUiState = InsertUiState(
+    insertUiEvent = toInsertUiEvent()
+)
+
+fun Kontak.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    id = id,
+    nama = nama,
+    alamat = email,
+    nohp = nohp,
+)
