a88d695a64fac9b0f13ebff2101529ca6fdfefc5
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2023-12-28 16:49:39 +0700

    add function EntryKontakBody

--- app/src/main/java/com/example/restapi/ui/kontak/screen/InsertScreen.kt
+++ app/src/main/java/com/example/restapi/ui/kontak/screen/InsertScreen.kt
@@ -1,15 +1,21 @@
 package com.example.restapi.ui.kontak.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.restapi.navigation.DestinasiNavigasi
 import com.example.restapi.ui.PenyediaViewModel
 import com.example.restapi.ui.TopAppBarKontak
+import com.example.restapi.ui.kontak.viewmodel.InsertUiEvent
+import com.example.restapi.ui.kontak.viewmodel.InsertUiState
 import com.example.restapi.ui.kontak.viewmodel.InsertViewModel
 
 
@@ -26,6 +32,21 @@ fun EntryKontakScreen(
     viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory),
 ){
 
+}
+
+@Composable
+fun EntryKontakBody(
+    insertUiState: InsertUiState,
+    onSiswaValueChange: (InsertUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+
+    }
 }
 
 
