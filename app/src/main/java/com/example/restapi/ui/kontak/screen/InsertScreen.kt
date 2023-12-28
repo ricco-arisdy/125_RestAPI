781fcf38c49c0c6321b4943b798b3b9b44b4c693
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2023-12-28 16:47:19 +0700

    add object DestinasiEntry and fun EntryKontakScreen

--- app/src/main/java/com/example/restapi/ui/kontak/screen/InsertScreen.kt
+++ app/src/main/java/com/example/restapi/ui/kontak/screen/InsertScreen.kt
@@ -5,13 +5,25 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.restapi.navigation.DestinasiNavigasi
+import com.example.restapi.ui.PenyediaViewModel
 import com.example.restapi.ui.TopAppBarKontak
+import com.example.restapi.ui.kontak.viewmodel.InsertViewModel
 
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Entry Siswa"
+}
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 
 fun EntryKontakScreen(
-
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory),
 ){
 
 }
