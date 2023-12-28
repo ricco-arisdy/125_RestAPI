f6629036bf6ddc448b6f5970562f91351232c929
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2023-12-28 17:35:44 +0700

    add Scaffold EntryKontakScreen in insert screen

--- app/src/main/java/com/example/restapi/ui/kontak/screen/InsertScreen.kt
+++ app/src/main/java/com/example/restapi/ui/kontak/screen/InsertScreen.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
@@ -14,6 +16,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -23,6 +26,7 @@ import com.example.restapi.ui.TopAppBarKontak
 import com.example.restapi.ui.kontak.viewmodel.InsertUiEvent
 import com.example.restapi.ui.kontak.viewmodel.InsertUiState
 import com.example.restapi.ui.kontak.viewmodel.InsertViewModel
+import kotlinx.coroutines.launch
 
 
 object DestinasiEntry : DestinasiNavigasi {
@@ -37,8 +41,35 @@ fun EntryKontakScreen(
     modifier: Modifier = Modifier,
     viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory),
 ){
-
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBarKontak(
+                title = DestinasiEntry.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }) { innerPadding ->
+        EntryKontakBody(
+            insertUiState = viewModel.insertKontakState,
+            onSiswaValueChange = viewModel::updateInsertKontakState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertKontak()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
 }
+
 
 @Composable
 fun EntryKontakBody(
